f998697ec58f2adf6f1cb21ffba9613a58f13b4c
jmanuelbello30 <jmanuebello30@gmail.com>
2020-04-03 18:39:32 -0300

    Adelanto de Registro Profesional

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_jwt_extended import (
 )
 from werkzeug.utils import secure_filename
 from flask_socketio import SocketIO, send
-from models import db, User, Professional, Patient, Perfil_Professional, Panic_Alert, Status_Professional, Message_Sent, Chat_Room
+from models import db, User, Professional, Patient, Panic_Alert, Status_Professional, Message_Sent, Chat_Room
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static')
@@ -98,11 +98,11 @@ def register():
     if not request.files:
         return jsonify({"msg": "Missing FILES in request"}), 400
 
-    file = request.files['avatar']
+    file = request.files['avatar'] # TO DO: cambiar el nombre por el de los documentos
     username = request.form.get('username', None)
     password = request.form.get('password', None)
 
-    if not file and file.filname == "":
+    if not file and file.filename == "":
         return jsonify({"msg": "Missing avatar parameter"}), 400
     if not username and username == "":
         return jsonify({"msg": "Missing username parameter"}), 400
@@ -134,11 +134,11 @@ def register():
     }
     return jsonify(data), 200
 
-@app.route('/api/send-lert')
+@app.route('/api/send-lert') # TO DO: corregir texto alert
 def send_alert():
 
     msg = Message("Prueba de Email", 
-        sender="helpmn2020@gmail.com",
+        sender="helpmn2020@gmail.com", # TO DO:  cambiar esto para probar
         recipients=[''])
 
     msg.body = "Hola esto es una prueba de email"
@@ -172,7 +172,158 @@ def handleMessage(msg):
     send(msg, broadcast=True)
     return None
 
- 
+# RUTAS DE PROFESIONALES
+
+@app.route('/api/professional/register', methods=['POST'])
+def professional_register():
+    # Data recibida:
+    #   request.name
+    #   request.email
+    #   request.password
+    #   request.files.rut
+    #   request.files.certification
+    #   request.files.numberid
+    #   request.files.courses
+
+    #print(request.files)
+
+    if not request.files:
+       return jsonify({"message": "Debe Seleccionar los documentos"}), 400
+
+    name          = request.form.get('name', None)
+    email         = request.form.get('email', None)
+    password      = request.form.get('password', None)
+
+    rut           = request.files["rut"]
+    certification = request.files["certification"]
+    numberid      = request.files["numberid"]
+    #courses       = request.files["courses"]
+
+    if not rut or rut.filename == "":
+        return jsonify({
+            "user": {},
+            "register": {
+                "error": "Debe cargar el documento RUT",
+                "message": "Datos con error",
+                "finish": "false"
+            }
+        }), 400
+    if not certification or certification.filename == "":
+        return jsonify({
+            "user": {},
+            "register": {
+                "error": "Debe cargar el documento certification",
+                "message": "Datos con error",
+                "finish": "false"
+            }
+        }), 400
+    if not numberid or numberid.filename == "":
+        return jsonify({
+            "user": {},
+            "register": {
+                "error": "Debe cargar el documento numberid",
+                "message": "Datos con error",
+                "finish": "false"
+            }
+        }), 400
+    #if courses and courses.filename == "":
+    #    return jsonify({
+    #        "user": {},
+     #       "register": {
+     #           "error": "Debe cargar el documento courses",
+      #          "message": "Datos con error",
+      #          "finish": "false"
+       #     }
+        #}), 400
+
+    if not name or name == "":
+        return jsonify({
+            "user": {},
+            "register": {
+                "error": "El nombre es obligatorio",
+                "message": "Datos con error",
+                "finish": "false"
+            }
+        }), 400
+    if not password or password == "":
+        return jsonify({
+            "user": {},
+            "register": {
+                "error": "Missing password parameter",
+                "message": "Datos con error",
+                "finish": "false"
+            }
+        }), 400
+    if not email or email == "":
+        return jsonify({
+            "user": {},
+            "register": {
+                "error": "Missing email parameter",
+                "message": "Datos con error",
+                "finish": "false"
+            }
+        }), 400
+
+
+    user = User.query.filter_by(email = email).first()
+    if user:
+        return jsonify({
+            "user": {},
+            "register": {
+                "error": "Ya existe un usuario con el email, ingrese otro email",
+                "message": "Datos con error",
+                "finish": "false"
+            }
+        }), 400
+
+    if allowed_file(rut.filename):
+        filename_rut = secure_filename(rut.filename)
+        rut.save(os.path.join(os.path.join(app.config['UPLOAD_FOLDER'], 'img/documents'), filename_rut))
+    if allowed_file(certification.filename):
+        filename_certification = secure_filename(certification.filename)
+        certification.save(os.path.join(os.path.join(app.config['UPLOAD_FOLDER'], 'img/documents'), filename_certification))
+    if allowed_file(numberid.filename):
+        filename_numberid = secure_filename(numberid.filename)
+        numberid.save(os.path.join(os.path.join(app.config['UPLOAD_FOLDER'], 'img/documents'), filename_numberid))
+    #if courses and allowed_file(courses.filename):
+     #   filename_courses = secure_filename(courses.filename)
+       # courses.save(os.path.join(os.path.join(app.config['UPLOAD_FOLDER'], 'img/documents'), filename_courses))
+
+    # Pasaron todas las validaciones
+    # Se registra el profesional en 3 pasos:
+
+    # 1. Se crea el profesional
+    new_professional = Professional()
+    new_professional.name = name
+    new_professional.rut = filename_rut
+    db.session.add(new_professional)
+    db.session.commit()
+    professional = new_professional.serialize()
+
+    # 2. Se registrará el usuario
+    user = User()
+    user.password = bcrypt.generate_password_hash(password)
+    user.email = email
+    user.professional_id = professional['id']
+    db.session.add(user)
+    db.session.commit()
+
+    # Luego de la creacion del usuario, se general el token de acceso para el login
+    access_token = create_access_token(identity=user.email)
+
+    # Respuesta
+    data = {
+        "access_token": access_token,
+        "user": user.serialize(),
+        "register": {
+            "error": "",
+            "message": "Profesional registrado con éxito",
+            "finish": "true"
+        }
+    }
+    return jsonify(data), 200
+
+
 if __name__ == '__main__':
     manager.run()
     
